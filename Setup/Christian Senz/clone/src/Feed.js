4f192284dab07b2c3279f54cc85b4ab3bda894de
csenz <casenz@hawaii.edu>
2020-10-23 14:51:59 -0400

    Added a backend for post functionality

--- Setup/Christian Senz/clone/src/Feed.js
+++ Setup/Christian Senz/clone/src/Feed.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Feed.css";
 import DogBox from "./DogBox";
 import Post from "./Post";
-
+import database from "./firebase";
 
 function Feed() {
+  const[dogs, setDogs]=useState([]);
+  useEffect(()=>{
+    database.collection("dogs").onSnapshot(snapshot =>(
+    setDogs(snapshot.docs.map(doc=>doc.data()))
+  ))
+  }, []);
   return (
     <div className="feed">
         <div className="feed__header">
         <h2>Home</h2>
         </div>
-       <DogBox /> 
-       <Post />
-       <Post />
-    </div>
+        <DogBox /> 
+      {dogs.map(dog => (
+       <Post name={dog.name} verified={dog.verified} text={dog.text} image={dog.image} avatar={dog.avatar}/>
+      ))}
+       </div>
   );
 }
 
